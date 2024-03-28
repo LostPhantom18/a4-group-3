0c5cbcf0a02d096bc86a12983ae7d925ac56581d
Eamoncowley <eamoncowley22@gmail.com>
2024-03-28 11:51:51 -0400

    Added Timer. Changed namespace

--- MazeGame/Program.cs
+++ MazeGame/Program.cs
@@ -1,8 +1,9 @@
 ﻿// Created by Jonah, Makayla, Eamon, Sudhan and Param
-//test
+// Test
 using Raylib_cs;
 using System;
 using System.Diagnostics;
+//using System.Drawing;
 using System.Numerics;
 
 namespace MazeGame
@@ -24,11 +25,18 @@ namespace MazeGame
         public static int numOfTilesToMove = 4;
         public static int currentTile = 0;
 
+        // Ball variables
+        const int ballRadius = 20;
+        const float ballSpeed = 200f;
+        public static Vector2 ballPosition = new Vector2(width / 2, height / 2);
+
         // Game completed check variables
         public static bool gameOneCompleted = false;
         public static bool gameTwoCompleted = false;
         public static bool gameThreeCompleted = false;
 
+        static Stopwatch timer = new Stopwatch();
+        static TimeSpan elapsedTime;
         static void Main(string[] args)
         {
             Raylib.InitWindow(width, height, title);
@@ -52,17 +60,58 @@ namespace MazeGame
             if (gameOneCompleted == false)
             {
                 drawLevelOne();
+                UpdateBall();
             }
             else if (gameOneCompleted && gameTwoCompleted == false)
             {
                 drawLevelTwo();
+                UpdateBall();
             }
             else if (gameOneCompleted && gameTwoCompleted && gameThreeCompleted == false)
             {
                 drawLevelThree();
+                UpdateBall();
+            }
+            if (gameOneCompleted && gameTwoCompleted && gameThreeCompleted)
+            {
+                timer.Stop();
             }
         }
+        static void UpdateBall()
+        {
+            Vector2 ballDirection = new Vector2(0, 0);
 
+            if (Raylib.IsKeyDown(KeyboardKey.D))
+            {
+                ballDirection = new Vector2(1, 0);
+            }
+            else if (Raylib.IsKeyDown(KeyboardKey.A))
+            {
+                ballDirection = new Vector2(-1, 0);
+            }
+            else if (Raylib.IsKeyDown(KeyboardKey.W))
+            {
+                ballDirection = new Vector2(0, -1);
+            }
+            else if (Raylib.IsKeyDown(KeyboardKey.S))
+            {
+                ballDirection = new Vector2(0, 1);
+            }
+
+            ballPosition += ballDirection * ballSpeed * Raylib.GetFrameTime();
+
+            if (ballPosition.X > width)
+                ballPosition.X = 0;
+            else if (ballPosition.X < 0)
+                ballPosition.X = width;
+
+            if (ballPosition.Y > height)
+                ballPosition.Y = 0;
+            else if (ballPosition.Y < 0)
+                ballPosition.Y = height;
+
+            Raylib.DrawCircle((int)ballPosition.X, (int)ballPosition.Y, ballRadius, Color.Yellow);
+        }
         static void drawLevelOne()
         {
             currentX = startX;
@@ -139,8 +188,8 @@ namespace MazeGame
                     break;
 
                 }
-                a = +50;
-                Raylib.DrawRectangle(currentX, currentY, 50, 50, Color.Gray);
+                currentX += 50;
+                Raylib.DrawRectangle(currentX, currentY, 50, 50, Color.Blue);
             }
             return;
         }
@@ -157,8 +206,8 @@ namespace MazeGame
                     a = 0;
                     break;
                 }
-                a = -50;
-                Raylib.DrawRectangle(currentX, currentY, 50, 50, Color.Gray);
+                currentX -= 50;
+                Raylib.DrawRectangle(currentX, currentY, 50, 50, Color.Blue);
             }
             return;
         }
@@ -175,8 +224,9 @@ namespace MazeGame
                     a = 0;
                     break;
                 }
-                a = +50;
-                Raylib.DrawRectangle(currentX, currentY, 50, 50, Color.Gray);
+
+                currentY += 50;
+                Raylib.DrawRectangle(currentX, currentY, 50, 50, Color.Blue);
             }
             return;
         }
@@ -193,10 +243,16 @@ namespace MazeGame
                     a = 0;
                     break;
                 }
-                a = -50;
-                Raylib.DrawRectangle(currentX, currentY, 50, 50, Color.Gray);
+                currentY -= 50;
+                Raylib.DrawRectangle(currentX, currentY, 50, 50, Color.Blue);
             }
             return;
+        }
+        static void DrawTimer()
+        {
+            elapsedTime = timer.Elapsed;
+            string timerText = $"Time: {elapsedTime.Minutes:00}:{elapsedTime.Seconds:00}.{elapsedTime.Milliseconds / 10:00}";
+            Raylib.DrawText(timerText, 10, 10, 20, Color.White);
         }
     }
 }
